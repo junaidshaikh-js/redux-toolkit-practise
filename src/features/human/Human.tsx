2201872d6945148eb03365025ed285d65ef086ca
Junaid <sj.shaikhjunaid@gmail.com>
2022-07-15 12:53:10 +0530

    display task assigned to humans

--- src/features/human/Human.tsx
+++ src/features/human/Human.tsx
@@ -1,3 +1,6 @@
+import { useAppSelector } from "../../app/hooks";
+import { Task } from "../task/Task";
+import { taskType } from "../task/taskSlice";
 import { HumanType } from "./humanSlice";
 
 type HumanPropType = {
@@ -5,9 +8,25 @@ type HumanPropType = {
 };
 
 export const Human = ({ human }: HumanPropType) => {
+  const { tasks } = useAppSelector((state) => state.tasks);
+
   return (
     <article className="my-2 px-1 py-2 border-2 border-slate-900 ">
       <h3 className="font-bold text-xl">{human.name}</h3>
+
+      {human.tasksIds.length !== 0 && (
+        <div>
+          <h4 className="my-4 text-center font-bold">Tasks</h4>
+
+          {human.tasksIds.map((_taskId) => {
+            const task = tasks.find(
+              (_task) => _task.id === _taskId
+            ) as taskType;
+
+            return <Task task={task} />;
+          })}
+        </div>
+      )}
     </article>
   );
 };
